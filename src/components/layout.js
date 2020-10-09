44eddec47d0a9d57d5666311992e03de7c00f773
Daniel Sauble <djsauble@gmail.com>
2020-10-09 15:32:48 -0700

    Better define the content space visually

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,11 @@
 import React from "react"
 import "./layout.css"
+import HeaderImg from "../images/top_left_border.png"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <header><div className="header-image"></div></header>
       <main>{children}</main>
     </>
   )
